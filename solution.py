486e0df58340be9687e3f28c145cb12ccdf1648d
hy072586 <98764128+hy072586@users.noreply.github.com>
2022-03-05 13:38:49 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,36 +5,43 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
     mailserver = ('127.0.0.1', 1025)
 
-    clientSocket = socket(AF_INET,SOCK_STREAM)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect(mailserver)
 
-    recv = clientSocket.recv(1024).decode()
-   
+    recv = clientSocket.recv(1024)
+
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    recv1 = clientSocket.recv(1024).decode()
-    
+    recv1 = clientSocket.recv(1024)
+
     # Send MAIL FROM command and handle server response.
-    clientSocket.send('MAIL FROM:<alice@example.edu>\r\n')
-    recv1=clientSocket.recv(1024)
-    
+    mailfrom = "MAIL FROM:<alice@example.edu>\r\n"
+    clientSocket.send(mailfrom.encode())
+    recv1 = clientSocket.recv(1024)
+
     # Send RCPT TO command and handle server response.
-    clientSocket.send('RCPT TO:<hy2245@nyu.edu>\r\n')
+    rcptTo = "RCPT TO:<hy2245@nyu.edu>\r\n"
+    clientSocket.send(rcptTo.encode())
     recv1 = clientSocket.recv(1024)
-   
+
     # Send DATA command and handle server response.
-    clientSocket.send('DATA\r\n')
-    recv1 = clientSocket.recv(1024)
-    
-    # Send message data.
     data = "DATA\r\n"
-    clientSocket.send(data.encode)
-      
-    # Message ends with a single period, send message end and handle server response.
+    clientSocket.send(data.encode())
+    recv1 = clientSocket.recv(1024)
+
+    # Send message data.
+    subject = "Subject: SMTP mail client testing \r\n\r\n"
+    clientSocket.send(subject.encode())
+    message = "Enter your message: \r\n"
+    clientSocket.send(message.encode())
     clientSocket.send(msg.encode())
+    recv_msg = clientSocket.recv(1024)
+
+    # Message ends with a single period, send message end and handle server response.
     clientSocket.send(endmsg.encode())
-    
+    recv1 = clientSocket.recv(1024)
+
     # Send QUIT command and handle server response.
     clientSocket.send('QUIT\r\n')
     clientSocket.close()
